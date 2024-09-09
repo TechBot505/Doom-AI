ab3fc1943796e3ba604e2b18071af00657fea4c5
Rohit <rohitdhanotia2003@gmail.com>
2024-09-09 12:45:18 +0530

    UI Improvements

--- frontend/app/dashboard/sql-chat/page.tsx
+++ frontend/app/dashboard/sql-chat/page.tsx
@@ -12,7 +12,7 @@ function SQLChat() {
     <div className="flex flex-col items-center dark:bg-darkPrimary bg-white gap-2 z-0">
       <div className="relative pt-12">
         <div className="relative flex flex-col items-center max-w-screen-xl mx-auto px-8 text-center">
-          <h1 className="bg-gradient-to-br from-amber-300 to to-orange-600 text-transparent bg-clip-text text-3xl md:text-4xl font-bold leading-tight mb-6">
+          <h1 className="bg-gradient-to-br from-amber-300 to to-orange-600 text-transparent bg-clip-text text-3xl md:text-5xl font-bold leading-tight mb-6">
             Chat with MySQL Database
           </h1>
           <p className="text-gray-500 dark:text-white text-lg max-w-2xl text-center mb-8">
